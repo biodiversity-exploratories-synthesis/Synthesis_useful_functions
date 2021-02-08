5697fe110344c2d53be9d1a8fcc66bb9fd919fe7
noelle <noelle.schenk@orss.ch>
2021-02-08 11:28:20 +0100

    added 3 more examples

--- 2021_synthesis_Rhelpdesk_good_programming_practices.R
+++ 2021_synthesis_Rhelpdesk_good_programming_practices.R
@@ -35,3 +35,67 @@ info_data <- data.table::fread("example.csv", header = T)
 info_data <- data.table::fread("example.csv", header = T)
 
 
+###
+# EXAMPLE 6 - PLAN YOUR PROGRAM
+#
+# Describe your problem and the steps you need to do to solve it.
+# Example problem : Plot a normal distribution with mean = 0 and sd = 1.
+# Example planning : 
+#    - generate a normally distributed dataset with mean = 0 and sd = 1
+#        - search internet which function can generate normally distributed data
+#        - look up help of this function in R
+#        - try out function
+#        - make vector x with normally distributed data
+#    - make the plot
+#        - search internet which function to use for plotting
+#        - make title and axis labels
+
+help(rnorm)
+rnorm(4, mean = 0, sd = 1)
+x <- rnorm(40, mean = 0, sd = 1) # first parameter : length of vector
+plot(x)
+# plot() just shows the data.points. Maybe a Histogram would fit better?
+#TODO plot histogram. 
+
+
+###
+# EXAMPLE 9 - USE A CODE STYLE
+#
+# examples taken from tidyverse code style 
+# http://adv-r.had.co.nz/Style.html and https://style.tidyverse.org/syntax.html#long-lines
+
+# Good
+average <- mean(feet / 12 + inches, na.rm = TRUE)
+# Bad
+average<-mean(feet/12+inches,na.rm=TRUE)
+
+# Good
+do_something_very_complicated(
+  something = "that",
+  requires = many,
+  arguments = "some of which may be long"
+)
+# Bad
+do_something_very_complicated("that", requires, many, arguments,
+                              "some of which may be long"
+)
+
+
+
+###
+# NAMING THINGS
+#
+# good names reduce comments
+60 <- time_tolerance # seconds
+60 <- time_tolerance_in_seconds
+
+
+###
+# READ ERROR MESSAGES
+#
+# read the error messages
+x <- as.factor("A", "B", "C")
+help("as.factor")
+# the function encodes a vector, I gave the elements of a vector,
+#    not the vector itself.
+x <- as.factor(c("A", "B", "C"))
